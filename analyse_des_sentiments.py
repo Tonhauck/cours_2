6062485d2c8528c13ba3b6aeb73638fd7c11f12d
Tonhauck <37869103+Tonhauck@users.noreply.github.com>
2020-12-10 12:24:19 +0100

    Update analyse_des_sentiments.py

--- analyse_des_sentiments.py
+++ analyse_des_sentiments.py
@@ -22,6 +22,7 @@ def remove_content(text):
     return text
 
 #Lecture du csv
+#A MODIFIER
 df = pd.read_csv('fichiers/all_tweets_realDonaldTrump_cleaned.csv')
 
 #creation d'un autre champs sans steminisation
@@ -36,9 +37,11 @@ df['subject']=df['tweet_sans_residu'].apply(lambda x: TextBlob(x).sentiment[1])
 #polarité positive ou négative selon le sentiment
 df['polarity']=df['sentiment'].apply(lambda x: 'pos' if x>=0 else 'neg')
 #sauvegarde du résultat en csv
+#A MODIFIER
 df.to_csv('all_tweets_realDonaldTrump_sentiment_analisis.csv',index=False, encoding='utf-8-sig')
 
 #Elaboration de graphique pour exploration de la donneé
 # fig=px.histogram(df[df['subject']>0.5], x='polarity', color='polarity')
 # fig=px.histogram(df[df['sentiment']>-1], x='polarity', color='polarity')
 # fig.show()
+
